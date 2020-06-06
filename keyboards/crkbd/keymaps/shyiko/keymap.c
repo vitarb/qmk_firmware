69b8684412449fd4f0e30b02e33eddc803a0beb8
Stanley Shyiko <stanley.shyiko@gmail.com>
2020-06-06 08:01:48 -0700

    Update keymap (add lower_shirt & extra layers)

--- keyboards/crkbd/keymaps/shyiko/keymap.c
+++ keyboards/crkbd/keymaps/shyiko/keymap.c
@@ -3,23 +3,57 @@
 // layers
 #define L_DEFAULT 0
 #define L_LOWER 1
-#define L_RAISE 2
-#define L_ADJUST 3 // LOWER+RAISE
+#define L_LOWER_SHIFT 2
+#define L_RAISE 3
+#define L_EXTRA 4
+#define L_ADJUST 5 // LOWER+RAISE
 
 // custom key codes for layer switching
 enum kc_custom {
   LOWER = SAFE_RANGE,
-  RAISE
+  RAISE,
+  EXTRA
 };
 
 #include "keymap_layers.h"
 
+bool restore_shift = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (restore_shift) {
+    register_code(KC_LSFT);
+    restore_shift = false;
+  }
   switch (keycode) {
+    case KC_MPRV: case KC_MNXT: case KC_PGUP:
+    case KC_MPLY: case KC_HOME: case KC_PGDOWN: case KC_END:
+      if (!IS_LAYER_ON(L_LOWER_SHIFT) || IS_LAYER_ON(L_RAISE)) {
+        break;
+      }
+      if (record->event.pressed) {
+        unregister_code(KC_LSFT);
+      } else {
+        restore_shift = true;
+      }
+      break; // let qmk handle keycode as usual
+    case KC_LSFT:
+      if (!IS_LAYER_ON(L_LOWER)) {
+        break;
+      }
+      if (record->event.pressed) {
+        layer_on(L_LOWER_SHIFT);
+      } else {
+        layer_off(L_LOWER_SHIFT);
+      }
+      break; // let qmk handle keycode as usual
     case LOWER:
       if (record->event.pressed) {
         layer_on(L_LOWER);
+        if (get_mods() & MOD_BIT(KC_LSFT)) {
+          layer_on(L_LOWER_SHIFT);
+        }
       } else {
+        layer_off(L_LOWER_SHIFT);
         layer_off(L_LOWER);
       }
       update_tri_layer(L_LOWER, L_RAISE, L_ADJUST);
@@ -31,6 +65,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(L_RAISE);
       }
       update_tri_layer(L_LOWER, L_RAISE, L_ADJUST);
+      return false;
+    case EXTRA:
+      if (record->event.pressed) {
+        layer_on(L_EXTRA);
+      } else {
+        layer_off(L_EXTRA);
+      }
       return false;
   }
   return true;
--- keyboards/crkbd/keymaps/shyiko/keymap_layers.h
+++ keyboards/crkbd/keymaps/shyiko/keymap_layers.h
@@ -1,42 +1,60 @@
 // QMK key codes
-// https://beta.docs.qmk.fm/features/keycodes_basic
+// https://beta.docs.qmk.fm/using-qmk/simple-keycodes/keycodes
+//
+// _______ == KC_TRNS (transparent, i.e. pass-through to lower layer)
+// XXXXXXX == KC_NO (noop)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-#ifdef LAYOUT_COLEMAK_DH    
-  [L_DEFAULT] = LAYOUT(                                                                                   \
-    KC_TAB,  KC_Q, KC_W, KC_F,    KC_P,  KC_B,   /**/ KC_J,   KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_BSPC,   \
-    KC_LSFT, KC_A, KC_R, KC_S,    KC_T,  KC_G,   /**/ KC_K,   KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT,   \
-    KC_LCTL, KC_Z, KC_X, KC_C,    KC_D,  KC_V,   /**/ KC_M,   KC_H,  KC_COMM, KC_DOT, KC_SLSH, KC_BSLASH, \
-                         KC_LGUI, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LALT                              \
+#ifdef LAYOUT_COLEMAK_DH
+  [L_DEFAULT] = LAYOUT(                                                                                 \
+    KC_TAB,  KC_Q, KC_W, KC_F,    KC_P,  KC_B,   /**/ KC_J,   KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_BSPC, \
+    KC_LSFT, KC_A, KC_R, KC_S,    KC_T,  KC_G,   /**/ KC_K,   KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT, \
+    KC_LCTL, KC_Z, KC_X, KC_C,    KC_D,  KC_V,   /**/ KC_M,   KC_H,  KC_COMM, KC_DOT, KC_SLSH, EXTRA,   \
+                         KC_LALT, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LGUI                            \
   ),
 #else
-  [L_DEFAULT] = LAYOUT(                                                                                   \
-    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC,   \
-    KC_LSFT, KC_A, KC_S, KC_D,    KC_F,  KC_G,   /**/ KC_H,   KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,   \
-    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSLASH, \
-                         KC_LGUI, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LALT                              \
+  [L_DEFAULT] = LAYOUT(                                                                                 \
+    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,  KC_I,    KC_O,   KC_SCLN, KC_BSPC, \
+    KC_LSFT, KC_A, KC_S, KC_D,    KC_F,  KC_G,   /**/ KC_H,   KC_J,  KC_K,    KC_L,   KC_P,    KC_QUOT, \
+    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, EXTRA,   \
+                         KC_LALT, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LGUI                            \
   ),
 #endif
 
   [L_LOWER] = LAYOUT(                                                                                                              \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    /**/ KC_6,      KC_7,     KC_8,      KC_9,    KC_0,        KC_BSPC,     \
-    KC_LSFT, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRID,  KC_BRIU, /**/ KC_MINUS,  KC_EQUAL, KC_PGUP,   XXXXXXX, KC_LBRACKET, KC_RBRACKET, \
-    KC_LCTL, KC_MPLY, KC_MPRV, KC_MNXT, KC_GRAVE, XXXXXXX, /**/ KC_INSERT, KC_HOME,  KC_PGDOWN, KC_END,  KC_SLSH,     KC_BSLASH,   \
-                               KC_LGUI, LOWER,    KC_SPC,  /**/ KC_ENT,    RAISE,    KC_LALT                                       \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,     /**/ KC_6,      KC_7,     KC_8,    KC_9,    KC_0,        _______,     \
+    _______, KC_VOLD, KC_VOLU, KC_UP,   KC_BSLASH, KC_GRAVE, /**/ KC_MINUS,  KC_EQUAL, XXXXXXX, XXXXXXX, KC_LBRACKET, KC_RBRACKET, \
+    _______, KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_ENT,   /**/ KC_INSERT, XXXXXXX,  KC_COMM, KC_DOT,  KC_SLSH,     _______,     \
+                               _______, _______,   _______,  /**/ _______,   _______,  _______                                     \
   ),
 
-  [L_RAISE] = LAYOUT(                                                                                                     \
-    KC_ESC,  KC_1,  KC_2,  KC_3,    KC_4,  KC_5,   /**/ KC_6,      KC_7,     KC_8,    KC_9,     KC_0,        KC_DELETE,   \
-    KC_LSFT, KC_F1, KC_F2, KC_F3,   KC_F3, KC_F5,  /**/ KC_MINUS,  KC_EQUAL, KC_UP,   KC_GRAVE, KC_LBRACKET, KC_RBRACKET, \
-    KC_LCTL, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10, /**/ KC_INSERT, KC_LEFT,  KC_DOWN, KC_RIGHT, KC_SLSH,     KC_BSLASH,   \
-                           KC_LGUI, LOWER, KC_SPC, /**/ KC_ENT,    RAISE,    KC_LALT                                      \
+  // NOTE: make sure to update keymap.c if you change this layer
+  [L_LOWER_SHIFT] = LAYOUT(                                                                                             \
+    _______, _______, _______, _______,   _______,  _______, /**/ _______, _______, _______, _______, _______, _______, \
+    _______, KC_MPRV, KC_MNXT, KC_PGUP,   _______,  _______, /**/ _______, _______, _______, _______, _______, _______, \
+    _______, KC_MPLY, KC_HOME, KC_PGDOWN, KC_END,   _______, /**/ _______, _______, _______, _______, _______, _______, \
+                               _______,   _______,  _______, /**/ _______, _______, _______                             \
+  ),
+
+  [L_RAISE] = LAYOUT(                                                                                                             \
+    _______, KC_F1,  KC_F2,  KC_F3,   KC_F4,     KC_F5,    /**/ KC_F6,     KC_F7,    KC_F8,   KC_F9,    KC_F10,      KC_DELETE,   \
+    _______, KC_F11, KC_F12, XXXXXXX, KC_BSLASH, KC_GRAVE, /**/ KC_MINUS,  KC_EQUAL, KC_UP,   KC_APP,   KC_LBRACKET, KC_RBRACKET, \
+    _______, KC_Z,   KC_X,   KC_C,    KC_V,      KC_ENT,   /**/ KC_INSERT, KC_LEFT,  KC_DOWN, KC_RIGHT, KC_SLSH,     _______,     \
+                             _______, _______,   _______,  /**/ _______,   _______,  _______                                      \
   ),
 
   [L_ADJUST] = LAYOUT(                                                                                                 \
-    XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,    KC_7,    KC_8,      KC_9,    KC_0,    XXXXXXX, \
-    KC_LSFT, KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, KC_PGUP,   XXXXXXX, XXXXXXX, XXXXXXX, \
-    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, KC_HOME, KC_PGDOWN, KC_END,  XXXXXXX, XXXXXXX, \
-                               KC_LGUI, LOWER,   KC_SPC,  /**/ KC_ENT,  RAISE,   KC_LALT                               \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, /**/ _______, _______, KC_PGUP,   XXXXXXX, _______, _______, \
+    _______, KC_Z,    KC_X,    KC_C,    KC_V,    _______, /**/ _______, KC_HOME, KC_PGDOWN, KC_END,  _______, _______, \
+                               _______, _______, _______, /**/ _______, _______, _______                               \
+  ),
+
+  [L_EXTRA] = LAYOUT(                                                                                                \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    KC_CAPS, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+                               _______, _______, _______, /**/ _______, _______, _______                             \
   )
 };
