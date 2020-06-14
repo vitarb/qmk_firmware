acacd2cc6d710d095f1c78851ebad8baa858aad5
Stanley Shyiko <stanley.shyiko@gmail.com>
2020-06-14 14:10:18 -0700

    Add NUMPAD layer

--- keyboards/crkbd/keymaps/shyiko/keymap_layers.h
+++ keyboards/crkbd/keymaps/shyiko/keymap_layers.h
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif
 
   [L_LOWER] = LAYOUT(                                                                                                              \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,     /**/ KC_6,      KC_7,     KC_8,    KC_9,    KC_0,        _______,     \
-    _______, KC_VOLD, KC_VOLU, KC_UP,   KC_BSLASH, KC_GRAVE, /**/ KC_MINUS,  KC_EQUAL, XXXXXXX, XXXXXXX, KC_LBRACKET, KC_RBRACKET, \
-    _______, KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_ENT,   /**/ KC_INSERT, XXXXXXX,  KC_COMM, KC_DOT,  KC_SLSH,     _______,     \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,     /**/ KC_6,      KC_7,     KC_8,    KC_9,        KC_0,        _______, \
+    _______, KC_VOLD, KC_VOLU, KC_UP,   KC_BSLASH, KC_GRAVE, /**/ KC_MINUS,  KC_EQUAL, XXXXXXX, KC_LBRACKET, KC_RBRACKET, _______, \
+    _______, KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_ENT,   /**/ KC_INSERT, XXXXXXX,  KC_LT,   KC_GT,       _______,     _______, \
                                _______, _______,   _______,  /**/ _______,   _______,  _______                                     \
   ),
 
@@ -37,24 +37,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                _______,   _______,  _______, /**/ _______, _______, _______                             \
   ),
 
-  [L_RAISE] = LAYOUT(                                                                                                             \
-    _______, KC_F1,  KC_F2,  KC_F3,   KC_F4,     KC_F5,    /**/ KC_F6,     KC_F7,    KC_F8,   KC_F9,    KC_F10,      KC_DELETE,   \
-    _______, KC_F11, KC_F12, XXXXXXX, KC_BSLASH, KC_GRAVE, /**/ KC_MINUS,  KC_EQUAL, KC_UP,   KC_APP,   KC_LBRACKET, KC_RBRACKET, \
-    _______, KC_Z,   KC_X,   KC_C,    KC_V,      KC_ENT,   /**/ KC_INSERT, KC_LEFT,  KC_DOWN, KC_RIGHT, KC_SLSH,     _______,     \
-                             _______, _______,   _______,  /**/ _______,   _______,  _______                                      \
+  [L_RAISE] = LAYOUT(                                                                                                              \
+    _______, KC_F1,  KC_F2,  KC_F3,   KC_F4,     KC_F5,    /**/ KC_F6,     KC_F7,    KC_F8,   KC_F9,       KC_F10,      KC_DELETE, \
+    _______, KC_F11, KC_F12, XXXXXXX, KC_BSLASH, KC_GRAVE, /**/ KC_MINUS,  KC_EQUAL, KC_UP,   KC_LBRACKET, KC_RBRACKET, _______,   \
+    _______, KC_Z,   KC_X,   KC_C,    KC_V,      KC_ENT,   /**/ KC_INSERT, KC_LEFT,  KC_DOWN, KC_RIGHT,    KC_SLSH,     KC_APP,    \
+                             _______, _______,   _______,  /**/ _______,   _______,  _______                                       \
   ),
 
   [L_ADJUST] = LAYOUT(                                                                                                 \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, /**/ _______, _______, KC_PGUP,   XXXXXXX, _______, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, /**/ _______, _______, KC_PGUP,   _______, _______, _______, \
     _______, KC_Z,    KC_X,    KC_C,    KC_V,    _______, /**/ _______, KC_HOME, KC_PGDOWN, KC_END,  _______, _______, \
                                _______, _______, _______, /**/ _______, _______, _______                               \
   ),
 
   [L_EXTRA] = LAYOUT(                                                                                                \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    KC_CAPS, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, \
+    KC_CAPS, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, NUMPAD,  \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
                                _______, _______, _______, /**/ _______, _______, _______                             \
+  ),
+
+  [L_NUMPAD] = LAYOUT(                                                                                                          \
+    _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH, KC_KP_ASTERISK, /**/ _______, _______, _______, _______, _______, _______, \
+    _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_MINUS, KC_KP_PLUS,     /**/ _______, _______, _______, _______, _______, _______, \
+    KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, NUMPAD,      _______,        /**/ _______, _______, _______, _______, _______, _______, \
+                               _______, _______,     _______,        /**/ _______, _______, _______                             \
   )
 };
