5a162355aef5a549e3b59646385c9049eb0033bb
Stanley Shyiko <stanley.shyiko@gmail.com>
2020-07-01 13:31:58 -0700

    Update keymap (allow switching between QWERTY and Colemak)

--- keyboards/crkbd/keymaps/shyiko/keymap_layers.h
+++ keyboards/crkbd/keymaps/shyiko/keymap_layers.h
@@ -6,48 +6,32 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-#ifdef LAYOUT_COLEMAK_DH
-  [L_DEFAULT] = LAYOUT(                                                                                 \
-    KC_TAB,  KC_Q, KC_W, KC_F,    KC_P,  KC_B,   /**/ KC_J,   KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_BSPC, \
-    KC_LSFT, KC_A, KC_R, KC_S,    KC_T,  KC_G,   /**/ KC_K,   KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT, \
-    KC_LCTL, KC_Z, KC_X, KC_C,    KC_D,  KC_V,   /**/ KC_M,   KC_H,  KC_COMM, KC_DOT, KC_SLSH, EXTRA,   \
-                         KC_LALT, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LGUI                            \
-  ),
-#elseif LAYOUT_COLEMAK
-  [L_DEFAULT] = LAYOUT(                                                                                 \
-    KC_TAB,  KC_Q, KC_W, KC_F,    KC_P,  KC_G,   /**/ KC_J,   KC_L,  KC_U,    KC_Y,   KC_SCLN, KC_BSPC, \
-    KC_LSFT, KC_A, KC_R, KC_S,    KC_T,  KC_D,   /**/ KC_H,   KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT, \
-    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,  KC_B,   /**/ KC_K,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, EXTRA,   \
-                         KC_LALT, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LGUI                            \
-  ),
-#elseif LAYOUT_QWERTY_SCLNP
-  [L_DEFAULT] = LAYOUT(                                                                                 \
-    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,  KC_I,    KC_O,   KC_SCLN, KC_BSPC, \
+  [L_QWERTY] = LAYOUT(                                                                                  \
+    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,  KC_I,    KC_O,   KC_INS,  KC_BSPC, \
     KC_LSFT, KC_A, KC_S, KC_D,    KC_F,  KC_G,   /**/ KC_H,   KC_J,  KC_K,    KC_L,   KC_P,    KC_QUOT, \
     KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, EXTRA,   \
                          KC_LALT, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LGUI                            \
   ),
-#else
-  [L_DEFAULT] = LAYOUT(                                                                                 \
-    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_BSPC, \
-    KC_LSFT, KC_A, KC_S, KC_D,    KC_F,  KC_G,   /**/ KC_H,   KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
-    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, EXTRA,   \
+
+  [L_COLEMAK] = LAYOUT(                                                                                 \
+    KC_TAB,  KC_Q, KC_W, KC_F,    KC_P,  KC_G,   /**/ KC_J,   KC_L,  KC_U,    KC_Y,   KC_INS,  KC_BSPC, \
+    KC_LSFT, KC_A, KC_R, KC_S,    KC_T,  KC_D,   /**/ KC_H,   KC_N,  KC_E,    KC_I,   KC_O,    KC_QUOT, \
+    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,  KC_B,   /**/ KC_K,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, EXTRA,   \
                          KC_LALT, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LGUI                            \
   ),
-#endif
 
   [L_LOWER] = LAYOUT(                                                                                                               \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,     /**/ KC_6,      KC_7,     KC_8,    KC_9,        KC_0,        _______,  \
-    _______, KC_VOLD, KC_VOLU, KC_UP,   KC_PIPE,   KC_UNDS,  /**/ KC_MINUS,  KC_EQUAL, KC_GT,   KC_LBRACKET, KC_RBRACKET, KC_GRAVE, \
-    _______, KC_MUTE, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_TILD,  /**/ KC_PLUS,   KC_LT,    _______, _______,     KC_BSLASH,   _______,  \
+    _______, KC_SCLN, KC_COLN, KC_UP,   KC_BSLASH, KC_UNDS,  /**/ KC_MINUS,  KC_EQUAL, KC_GT,   KC_LBRACKET, KC_RBRACKET, _______,  \
+    _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_TILD,  /**/ KC_PLUS,   KC_LT,    _______, _______,     _______,     KC_GRAVE, \
                                _______, _______,   _______,  /**/ _______,   _______,  _______                                      \
   ),
 
   // NOTE: make sure to update keymap.c if you change this layer
   [L_LOWER_SHIFT] = LAYOUT(                                                                                             \
     _______, _______, _______, _______,   _______,  _______, /**/ _______, _______, _______, _______, _______, _______, \
-    _______, KC_MPRV, KC_MNXT, KC_PGUP,   _______,  _______, /**/ _______, _______, _______, _______, _______, _______, \
-    _______, KC_MPLY, KC_HOME, KC_PGDOWN, KC_END,   _______, /**/ _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, KC_PGUP,   _______,  _______, /**/ _______, _______, _______, _______, _______, _______, \
+    _______, _______, KC_HOME, KC_PGDOWN, KC_END,   _______, /**/ _______, _______, _______, _______, _______, _______, \
                                _______,   _______,  _______, /**/ _______, _______, _______                             \
   ),
 
@@ -60,16 +44,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_ADJUST] = LAYOUT(                                                                                                 \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, KC_PGUP,   XXXXXXX, XXXXXXX, _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, KC_HOME, KC_PGDOWN, KC_END,  XXXXXXX, _______, \
+    _______, KC_BRID, KC_BRIU, KC_MUTE, KC_VOLD, KC_VOLU, /**/ XXXXXXX, XXXXXXX, KC_PGUP,   XXXXXXX, XXXXXXX, _______, \
+    _______, XXXXXXX, KC_PSCR, KC_MPLY, KC_MPRV, KC_MNXT, /**/ XXXXXXX, KC_HOME, KC_PGDOWN, KC_END,  XXXXXXX, _______, \
                                _______, _______, _______, /**/ _______, _______, _______                               \
   ),
 
-  [L_EXTRA] = LAYOUT(                                                                                                  \
-    _______,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, KC_INSERT, XXXXXXX, KC_PSCR, XXXXXXX, \
-    LSFT_T(KC_CAPS), KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
-    _______,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, _______, \
-                               _______, _______, _______,         /**/ _______, _______, _______                               \
+  [L_EXTRA] = LAYOUT(                                                                                                                   \
+    _______,         DF(L_QWERTY), XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    LSFT_T(KC_CAPS), XXXXXXX,      XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    _______,         XXXXXXX,      XXXXXXX, DF(L_COLEMAK), XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+                               _______, _______, _______,         /**/ _______, _______, _______                                        \
   ),
 
   [L_MOUSE] = LAYOUT(                                                                                                                           \
