0cd1ab6495b5ec0c62dfd62407970c7d277aad4d
Vitaly <Vitaly.Arbuzov@gmail.com>
2020-07-16 22:03:26 -0700

    swap lgui and extra

--- keyboards/crkbd/keymaps/vitarb/keymap_layers.h
+++ keyboards/crkbd/keymaps/vitarb/keymap_layers.h
@@ -9,19 +9,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_QWTY] = LAYOUT(																																\
     KC_TAB,		KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,    /**/ KC_Y,			KC_U,		KC_I,		KC_O,		KC_P,		KC_BSLS,\
     KC_LCTL,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,    /**/ KC_H,			KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,\
-    KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,	 /**/ KC_N,			KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	EXTRA,	\
-										KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,		RAISE,		KC_LGUI										\
+    KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,	 /**/ KC_N,			KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_LGUI,\
+										KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,		RAISE,		EXTRA										\
   ),
 
   [L_CLMK] = LAYOUT(																																\
     KC_TAB,		KC_Q,		KC_W,		KC_F,		KC_P,		KC_G,	 /**/ KC_J,			KC_L,		KC_U,		KC_Y,		KC_SCLN,	KC_BSLS,\
     KC_LCTL,	KC_A,		KC_R,		KC_S,		KC_T,		KC_D,    /**/ KC_H,			KC_N,		KC_E,		KC_I,		KC_O,		KC_QUOT,\
-    KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,    /**/ KC_K,			KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	EXTRA,  \
-										KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,		RAISE,		KC_LGUI										\
+    KC_LSFT,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,    /**/ KC_K,			KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_LGUI,\
+										KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,		RAISE,		EXTRA										\
   ),
 
   [L_LOWER] = LAYOUT(																																\
-    KC_ESC,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,	 /**/ KC_6,			KC_7,		KC_8,		KC_9,		KC_0,		_______,\
+    KC_ESC,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,	 /**/ KC_6,			KC_7,		KC_8,		KC_9,		KC_0,		KC_PIPE,\
     _______,	KC_EXLM,	KC_AT,		KC_HASH,	KC_DLR,		KC_PERC, /**/ KC_CIRC,		KC_AMPR,	KC_ASTR,	KC_LPRN,	KC_RPRN,	KC_GRV, \
     _______,	KC_LBRC,	KC_RBRC,	KC_LCBR,	KC_RCBR,	KC_UNDS, /**/ KC_MINS,		KC_EQL,		KC_LT,		KC_GT,		KC_PLUS,	_______,\
 										_______,	_______,	KC_LGUI, /**/ KC_BSPC,		_______,	_______										\
