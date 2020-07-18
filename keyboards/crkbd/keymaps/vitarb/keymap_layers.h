8bfcf6793d73801758ead928fbbffdbbafad5105
Vitaly <Vitaly.Arbuzov@gmail.com>
2020-07-18 13:42:59 -0700

    enable leader key

--- keyboards/crkbd/keymaps/vitarb/keymap_layers.h
+++ keyboards/crkbd/keymaps/vitarb/keymap_layers.h
@@ -10,14 +10,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,		KC_Q,			KC_W,		KC_E,		KC_R,		KC_T,    /**/ KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		KC_UNDS,\
     KC_LCTL,	KC_A,			KC_S,		KC_D,		KC_F,		KC_G,    /**/ KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	KC_QUOT,\
     KC_LSFT,	KC_Z,			KC_X,		KC_C,		KC_V,		KC_B,	 /**/ KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_LGUI,\
-											KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,	RAISE,		EXTRA										\
+											KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,	RAISE,		KC_LEAD										\
   ),
 
   [L_CLMK] = LAYOUT(																																\
     KC_TAB,		KC_Q,			KC_W,		KC_F,		KC_P,		KC_G,	 /**/ KC_J,		KC_L,		KC_U,		KC_Y,		KC_SCLN,	KC_UNDS,\
     KC_LCTL,	KC_A,			KC_R,		KC_S,		KC_T,		KC_D,    /**/ KC_H,		KC_N,		KC_E,		KC_I,		KC_O,		KC_QUOT,\
     KC_LSFT,	KC_Z,			KC_X,		KC_C,		KC_V,		KC_B,    /**/ KC_K,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	KC_LGUI,\
-											KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,	RAISE,		EXTRA										\
+											KC_LALT,	LOWER,		KC_SPC,  /**/ KC_ENT,	RAISE,		KC_LEAD										\
   ),
 
   [L_LOWER] = LAYOUT(																																\
--- keyboards/crkbd/keymaps/vitarb/rules.mk
+++ keyboards/crkbd/keymaps/vitarb/rules.mk
@@ -1,3 +1,4 @@
 # enable media (volume down, etc.) keys
 EXTRAKEY_ENABLE = yes
 MOUSEKEY_ENABLE = yes
+LEADER_ENABLE = yes
